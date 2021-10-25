ac4ef6272dd6a08e8a9b597827b097d6e0c3d7b1
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-25 22:51:34 +0100

    Commit teste

--- teste.java
+++ teste.java
@@ -6,5 +6,6 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("teste");
         System.out.println("teste2");
+        System.out.println("yo yoy oo yo");
     }
 }
