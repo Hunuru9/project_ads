fab610356221d5913afd50012df98d606fbc75f0
Hunuru9 <hacls@iscte-iul.pt>
2021-10-25 23:32:25 +0100

    Initial Commit

--- teste.java
+++ teste.java
@@ -5,7 +5,5 @@ public class teste {
 
     public static void main(String[] args) {
         System.out.println("teste");
-        System.out.println("teste2");
-        System.out.println("yo yoy oo yo");
     }
 }
