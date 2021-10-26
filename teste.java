f76a95eebcdc7cfaf5a1c2bf3061922e164dbcbb
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-26 11:28:25 +0100

    Teste Francisco

--- teste.java
+++ teste.java
@@ -6,6 +6,6 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("teste");
         System.out.println("teste2");
-        System.out.println("yo yoy oo yo");
+        System.out.println("teste3");
     }
 }
