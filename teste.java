43e2d0398b62df6aaf02680b14d868b1820d5d5b
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-26 11:34:15 +0100

    teste

--- teste.java
+++ teste.java
@@ -5,10 +5,7 @@ public class Teste {
 
     public static void main(String[] args) {
         System.out.println("teste");
-<<<<<<< HEAD
         System.out.println("teste2");
         System.out.println("teste3");
-=======
->>>>>>> b0f80c6c40045e4ac4757f8dd2057d818659516b
     }
 }
