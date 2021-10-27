bbdf89fa0ef7e244eb3b1ec2929998f6d8451bac
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-27 19:41:29 +0100

    Francisco - iscte a toa

---
+++ src/Room.java
@@ -0,0 +1,22 @@
+public class Room {
+
+    private String name;
+    private int capacidade;
+    private int capacidade_exame;
+    private int number_characteristics;
+    private List<Characteristic> characteristics;
+    
+    public Room(String name, int capacidade, int capacidade_exame, int number_characteristics, List<Characteristic> characteristics) {
+        this.name = name;
+        this.capacidade = capacidade;
+        this.capacidade_exame = capacidade_exame;
+        this.number_characteristics = number_characteristics;
+        this.characteristics = characteristics;
+    }
+    
+    
+
+
+    
+    
+}
