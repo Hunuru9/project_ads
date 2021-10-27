9bc7f3bd2e751b394a2a84f7f77d364aa975fa46
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-27 19:42:39 +0100

    iscte

---
+++ src/Building.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Building{
+
+    private List<Room> rooms;
+    private String name;
+
+    public Building(List<Room> rooms, String name){
+        this.rooms = rooms;
+        this.name = name;
+    }
+
+
+
+
+
+
+
+
+}
