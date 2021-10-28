157ca5ea13cd01a4aeaf1fa089dbc6bc45590e6e
fnpms-iscte <fnpms@iscte-iul.pt>
2021-10-28 22:50:34 +0100

    ExcelController

--- src/Room.java
+++ src/Room.java
@@ -4,9 +4,9 @@ public class Room {
     private int capacidade;
     private int capacidade_exame;
     private int number_characteristics;
-    private List<Characteristic> characteristics;
+    private List<String> characteristics;
     
-    public Room(String name, int capacidade, int capacidade_exame, int number_characteristics, List<Characteristic> characteristics) {
+    public Room(String name, int capacidade, int capacidade_exame, int number_characteristics, List<String> characteristics) {
         this.name = name;
         this.capacidade = capacidade;
         this.capacidade_exame = capacidade_exame;
